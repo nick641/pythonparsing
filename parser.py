99157ff2dac1b88a6f5d38904ae6fe50529c7b4c
nick641 <85549122+nick641@users.noreply.github.com>
2022-05-23 17:47:46 +0900

    repair
    .

--- parser.py
+++ parser.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup as bs
+import pandas as pd
+
 
 
 page = requests.get("https://library.gabia.com/")
@@ -7,5 +9,14 @@ soup = bs(page.text, "html.parser")
 
 elements = soup.select('div.esg-entry-content a.eg-grant-element-0')
 
-for index, element in enumerate(elements, 1):
-		print("{} 번째 게시글: {}, {}".format(index, element.text, element.attrs['href']))
+titles = []
+links = []
+for index,element in enumerate(elements, 1):
+    titles.append(element.text)
+    links.append(element.attrs['href'])
+
+df = pd.DataFrame()
+df['titles'] = titles
+df['links']  =links
+
+df.to_excel('./library_gabia.xlsx', sheet_name='Sheet1')
